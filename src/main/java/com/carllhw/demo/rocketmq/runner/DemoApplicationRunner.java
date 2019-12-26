8012b8faf7e3029d3e7210015104494805548045
Haiwei Liu <carllhw@gmail.com>
2019-12-26 10:03:15 +0800

    fix test
    Signed-off-by: Haiwei Liu <carllhw@gmail.com>

--- src/main/java/com/carllhw/demo/rocketmq/runner/DemoApplicationRunner.java
+++ src/main/java/com/carllhw/demo/rocketmq/runner/DemoApplicationRunner.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 /**
@@ -13,6 +14,7 @@ import org.springframework.stereotype.Component;
  * @author carllhw
  */
 @Slf4j
+@Profile("!test")
 @Component
 public class DemoApplicationRunner implements ApplicationRunner {
 
